420d7094a36ae3768d43ac9fc394ea7be7f2ac48
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-13 16:02:57 +0100

    final

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+//var portfolioAnimate = {
+
+	//textAnimate: function() {
+		//('h2').animate(left: "+50", 0.33);
+	//},
+
+	//changeProfileImage: function() {
+		//$('.profile-photo').click(
+			//$('img').remove();
+			//$('.profile-photo').append('<img src="img/love.jpg" class="profile-photo">');
+		//)
+	//}//
+
+
+//}
+
+//$(document).ready(portfolioAnimate.changeProfileImage);//
