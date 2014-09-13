c2ae50043fac8648ef5789c6f2133a41838b0575
subairifeoluwapo <subairifeoluwapo@gmail.com>
2014-09-13 16:38:05 +0100

    new

--- js/myscript.js
+++ js/myscript.js
@@ -1,8 +1,10 @@
-//var portfolioAnimate = {
+var portfolioAnimate = {
 
-	//textAnimate: function() {
-		//('h2').animate(left: "+50", 0.33);
-	//},
+		textAnimate: function() {
+			var divWidth = $("#logo").width() / 100 * 10;
+			$('h2').animate({'marginLeft': divWidth + "px"}, 4000);
+			$('h1').animate({'marginRight': divWidth + "px"}, 2000);
+		}
 
 	//changeProfileImage: function() {
 		//$('.profile-photo').click(
@@ -12,6 +14,6 @@
 	//}//
 
 
-//}
+}
 
-//$(document).ready(portfolioAnimate.changeProfileImage);//
+$(document).ready(portfolioAnimate.textAnimate);
